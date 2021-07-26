75f72901c29dfff45879dd1e051f882826126983
JvPelai <jv.pelai@gmail.com>
2021-07-26 19:36:11 -0300

    graphql + apolloServer express

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
-import path from "path";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-console.log(path.posix.dirname("."));
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  /*const post = orm.em.create(Post, { title: "first post!" });
-  await orm.em.persistAndFlush(post); 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts); */
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server running on localhost:4000");
+  });
 };
 
 main().catch((err) => {
